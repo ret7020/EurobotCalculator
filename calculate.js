4cd63227deb6dd74ef73fda303027af8987bedc5
ret7020 <ret7020@gmail.com>
2022-09-11 23:40:38 +0300

    Base points calculation; Backend

--- calculate.js
+++ calculate.js
@@ -1,114 +1,44 @@
-function calc(){
-        //Clear res every function call 
-        let res = 0;
-        let final_str = "";
-        
-        res += parseInt(document.getElementById("artifact_in").value) * 5;
-        final_str += "5 * " + document.getElementById("artifact_in").value + " ";
-        if (document.getElementById("statue_0").checked){
-            res += 2;
-            final_str += "+ 2 ";
-        }
+points_inputs_ids = ["fine_points_cnt", "cake_layers_cnt", "correct_cakes_cnt", "cakes_with_cherry", "cherries_in_basket_cnt"];
+checkboxes_ids = ["basketPlaced", "backetCounterWorks", "robotsReturn", "finalActionDone"];
 
-        if (document.getElementById("statue_1").checked){
-            res += 5;
-            final_str += "+ 5 ";
-        }
+function calculate_points(){
+    let final_points_cnt = 0;
+    // Number inputs
+    points_inputs_ids.forEach(element => {
+        let inp = document.getElementById(element);
+        final_points_cnt += inp.value * inp.dataset.pointsCoeff;
+    });
+    // Checkboxes
+    checkboxes_ids.forEach(element => {
+        let inp = document.getElementById(element);
+        final_points_cnt += inp.checked * inp.dataset.points;
+    });
 
-        if (document.getElementById("statue_2").checked){
-            res += 10;
-            final_str += "+ 10 ";
-        }
+    return final_points_cnt;
+}
 
-        if (document.getElementById("statue_3").checked){
-            res += 15;
-            final_str += "+ 15 ";
-        }
+function calculate_fines(){
+    let final_fine_cnt = 0;
+    for (let i = 0; i <= 5; i++) {
+        let checkbox = document.getElementById(`fine_${i}`);
+        final_fine_cnt += checkbox.checked * checkbox.dataset.fineSize;
+      }
+    return final_fine_cnt;
+}
 
-        if (document.getElementById("vitrin_0").checked){
-            res += 2;
-            final_str += "+ 2 ";
-        }
+function apply_fines(){
+    document.getElementById("fine_points_cnt").value = document.getElementById("fine_points_cnt_preview").textContent;
+}
 
-        if (document.getElementById("vitrin_1").checked){
-            res += 5;
-            final_str += "+ 5 ";
-        }
+function visualize_points(){
+    return 0;
+}
 
-        if (document.getElementById("finish").checked){
-            res += 20;
-            final_str += "+ 20 ";
-        }
-
-        if (document.getElementById("artifact_cross").checked)
-        {
-            if (document.getElementById("artifact_opened").value != ""){
-                res += parseInt(document.getElementById("artifact_opened").value) * 5;
-                final_str += "+ 5 * " + document.getElementById("artifact_opened").value + " "
-            }
-        }
-
-        if (document.getElementById("obraz_raspr").value != ""){
-            res += parseInt(document.getElementById("obraz_raspr").value);
-            final_str += "+ " + document.getElementById("obraz_raspr").value + " "
-        }
-
-        if (document.getElementById("obraz_in_lag").value != ""){
-            res += parseInt(document.getElementById("obraz_in_lag").value);
-            final_str += "+ " + document.getElementById("obraz_in_lag").value + " "
-        }
-
-        if (document.getElementById("obraz_in_lag_sorted").value != ""){
-            res += parseInt(document.getElementById("obraz_in_lag_sorted").value);
-            final_str += "+ " + document.getElementById("obraz_in_lag_sorted").value + " "
-        }
-
-        if (document.getElementById("obraz_in_gal").value != ""){
-            res += parseInt(document.getElementById("obraz_in_gal").value) * 3;
-            final_str += "+ 3 * " + document.getElementById("obraz_in_gal").value + " "
-        }
-
-        if (document.getElementById("obraz_in_gal_sorted").value != ""){
-            res += parseInt(document.getElementById("obraz_in_gal_sorted").value) * 3;
-            final_str += "+ 3 * " + document.getElementById("obraz_in_gal_sorted").value + " "
-        }
-
-
-
-        
-
-        
-        
-        
-        console.log(res);
-        let bonus = 0;
-        if (document.getElementById("predicted").value != ""){
-            let delta = Math.abs(parseInt(document.getElementById("predicted").value)  - res);
-            console.log(delta);
-            bonus = Math.round(0.3 * res - delta);
-            if (bonus < 0){
-                bonus = 0;
-            }
-            res += bonus
-            res += 1;
-            console.log(res);
-            final_str += "+ " + bonus + " + 1";
-        }else{
-            res += 1;
-            final_str += "+ " + bonus + " + 1";
-        }
-
-        if (parseInt(document.getElementById("fine").value) != 0 && document.getElementById("fine").value != ""){
-            //Do fine calc after bonus calculating
-            res -= parseInt(document.getElementById("fine").value);
-            final_str += " - " + document.getElementById("fine").value;
-        }
-
-        
-        
-        
-
-
-        document.getElementsByTagName("result")[0].style.display = "block";
-        document.getElementsByTagName("result")[0].innerHTML = "Результат: <b style='color: #00dd8a'>" + res + " </b>баллов<br>Без бонуса(и 1 балла): <b style='color: #c1dd00'>" + (res - bonus - 1) + "</b> баллов<br>Бонус: <b style='color: #00bedd'>" + bonus + "</b> балла<br>" + final_str;
-    }
+//Events bindings
+document.getElementById("fines_select_form").onchange = function(){
+    document.getElementById("fine_points_cnt_preview").textContent = calculate_fines();
+}
+document.getElementById("fineSelectModal").addEventListener('hide.bs.modal', function () {
+    apply_fines();
+})
+  
